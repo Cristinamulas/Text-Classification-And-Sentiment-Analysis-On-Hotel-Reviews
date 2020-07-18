e8e7413149c2af2e64fd08605e18e9c4b1470f80
cristinamulas <cristinamulaslopez@gmail.com>
2020-07-18 16:06:32 -0400

    adding countvectorizer notebook

--- functions.py
+++ functions.py
@@ -88,7 +88,7 @@ def clf_grid(model, df, col, col1, parameteres):
 def table_results(list_):
     """ convert a list of dict into a df """
     df_results = pd.DataFrame((i for i in list_) , columns=['Model','Accuracy_score', 'Recall_score','Precision_score','F1_score','Time'] )
-    df_results['Model'] = ['Logistic Regresion', 'Decision Tree Classifier', 'Mutinomial NB', 'Linear Support Vector Classification', 'Random Forest Classifier']
+    df_results['Model'] = ['Logistic Regresion', 'Decision Tree Classifier','Random Forest Classifier', 'Ada Boots', 'Gradient_boosting', 'XGB','Support Vector Machine', 'Mutinomial NB']
     df_results = df_results.set_index('Model')
     return df_results
 
@@ -110,8 +110,37 @@ def unnest_df(df):
     f1_score = table_manupulation(df_recall_pre_f1, 'F1_score')
     df_metrix = pd.DataFrame([recall, precision, f1_score]).transpose()
     df_metrix['target_class'] = pd.Series([1,2,3,4,5] * 5)
-    repeat_model = pd.Series(['Logistic Regresion', 'Decision Tree Classifier', 'Mutinomial NB', 'Linear Support Vector Classification', 'Random Forest Classifier'])
+    repeat_model = pd.Series(['Logistic Regresion', 'Decision Tree Classifier','Random Forest Classifier', 'Ada Boots', 'Gradient_boosting', 'XGB','Support Vector Machine', 'Mutinomial NB'])
     repeat_model = repeat_model.repeat(5).reset_index()
     df_metrix['model']  = repeat_model.drop(columns='index')
     return df_metrix
 
+
+def clf_count_vectorizer(model, df, col1, col):
+    start = timeit.timeit()
+    """It takes two columns and a classifier, split the data into traing and testing, vectorize using Count Vectorizer, remove irrlevant features using and implemnt a ca
+    classifier using a pipeline, return a dictionary with accuracy, precions and recall f_1 socre and running time"""
+
+    X_train, X_test, y_train, y_test = train_test_split(df[col1], df[col] , test_size=0.3, random_state = 42)
+
+    classification_model = Pipeline([('vect', CountVectorizer(binary=True)),
+                         ('chi',  SelectKBest(chi2, k=10000)),
+                   ('clf', model)])
+
+    classification_model.fit(X_train, y_train)
+    
+    preds = classification_model.predict(X_test)
+    end = timeit.timeit()
+    final_time = start - end
+
+    list_names = ['Accuracy_score','Recall_score','Precision_score','F1_score', 'Time']
+    score_list = []
+    accuracy = str(round(accuracy_score(y_test, preds), 2) * 100) + '%'
+    recall = recall_score(y_test, preds,average=None, pos_label='Good')
+    precision = precision_score(y_test, preds,average=None, pos_label='Good')
+    f_1 = f1_score(y_test , preds,average=None, pos_label='Good')
+    final_time = start - end
+
+    score_list.extend([accuracy,recall,precision,f_1,final_time])
+    dictionary = dict(zip(list_names, score_list))
+    return dictionary
